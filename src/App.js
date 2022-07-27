8d7009887b8e1bca8d981f5f1afc730ff81ad034
soufo.25 <nteyouyousoufo>
2022-07-27 14:45:15 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+class Welcome extends React.Component {
+  state = {Person:{fullName:'Nteyou Yousouf', bio: 'We demistify the web for you',
+  imgSrc:'', profession: 'Web Dev', 
+ },
+show: true, count: 0, intervalId: 0
+};
+  showStatePerson = () =>{this.setState({
+    ...this.state,
+    show: !this.state.show
+  });
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+componentDidMount() {
+  const intervalId = setInterval(() => {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }, 1000);
 }
 
-export default App;
+componentWillUnmount(){
+  clearInterval(this.state.intervalId);
+};
+
+
+
+render() {
+  return (
+    <>
+      {this.state.show && (
+        <>
+          <h1>{this.state.Person.fullName}</h1>
+          <h3>{this.state.Person.bio}</h3>
+          <img src={this.state.Person.imgSrc} alt=" " width ="40px" height= "40px"></img>
+          <h3>{this.state.Person.profession}</h3>
+          <br></br>
+        </>
+      )}
+
+      <button onClick={() => this.showStatePerson () }>click here</button>
+      <h2>Component render time: {this.state.count} seconds</h2>
+    </>
+    
+  );
+}
+}
+
+
+
+export default Welcome;
